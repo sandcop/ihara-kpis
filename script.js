fd5bf00808a1990099b3edf6495a55e9a48ca7b0
sandcop <145860117+sandcop@users.noreply.github.com>
2026-03-27 22:15:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ── CONFIG ──────────────────────────────────────────────
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxxRb-4KNDdhR-6di4vXbWwpos6eN1SMOo3j7SIktIfWWDOH3_er8MwjQSoAOwFPO8/exec'; 
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwzlzGfp2KUdQCdjMVuC78g7nCy-JHInXGVdCw1iaw9r-CQEBFaOOLEiM9n5a8E1NKg/exec'; 
 const FIREBASE_BUCKET = 'seguimiento-ventas-manuel.firebasestorage.app'; // ← o el del cliente
 const GALERIA_FOLDER  = 'galeria_v2/';
 const PASS_KEY        = btoa('Ventas2025'); // contraseñadeventas (sync con Config.gs)
